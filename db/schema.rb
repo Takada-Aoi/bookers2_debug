2659ec5d57efaa1001e0c8648e7cadeeea91c9dd
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-02 09:46:05 +0000

    [Add]2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_015925) do
+ActiveRecord::Schema.define(version: 2021_05_02_092644) do
+
+  create_table "book_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "book_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "books", force: :cascade do |t|
     t.string "title"
