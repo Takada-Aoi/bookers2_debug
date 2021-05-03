431d5ddfc8c82545bdf3b421d309fde7a1358265
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-03 04:34:10 +0000

    [Start]3

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,15 @@
 class BookCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+		@book = Book.find(params[:book_id])
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.user_id = current_user.id
+		if @book_comment.save
+  		redirect_to book_path(@book.id)
+		else
+		  render 'books/show'
+		end
   end
   
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,27 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
+  
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :follower, through: :relationships, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :relationships, source: :followed
+  
+  def follow(other_user)
+    following　<<　other_user
+  end 
+  
+  def unfollow(other_user)
+   relationships.find_by(followed_id:other_user.id).destroy
+  end
+  
+  def following?(other_user)
+   following.include?(other_user)
+  end 
+  
 end
