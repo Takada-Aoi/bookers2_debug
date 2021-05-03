16d7fe55d88d9312db73ee06ebba40b0a286ef02
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-03 14:45:44 +0000

    [Add]5

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,15 +14,15 @@ class User < ApplicationRecord
   validates :introduction, length: {maximum: 50}
   
   has_many :active_relationships,  class_name:  "Relationship",
-                                   foreign_key: "following_id",
+                                   foreign_key: "followed_id",
                                    dependent:   :destroy
   has_many :passive_relationships, class_name:  "Relationship",
                                    foreign_key: "follower_id",
                                    dependent:   :destroy
-  has_many :followings, through: :active_relationships,  source: :follower
-  has_many :followers, through: :passive_relationships, source: :following
+  has_many :followed, through: :active_relationships,  source: :follower
+  has_many :followers, through: :passive_relationships, source: :followed
 
  def followed_by?(user)
-  passive_relationships.find_by(following_id: user.id).present?
-end
+  passive_relationships.find_by(followed_id: user.id).present?
+ end
 end
