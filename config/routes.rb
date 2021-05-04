74a01cb1d3489c64afcbffc24f9681237a119dcb
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-04 08:24:32 +0000

    [Finish]4

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+  get '/search' => 'search#search'
   devise_for :users
   root 'homes#top'
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
-    get :follows, on: :member 
-    get :followers, on: :member 
+    get :follows, on: :member
+    get :followers, on: :member
   end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments,only:[:create,:destroy]
-  end  
-   
+  end
+
 end
