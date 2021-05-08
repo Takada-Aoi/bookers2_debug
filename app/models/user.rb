a2fd0c476d7c8bd7c0a2225cc5f82ed3de84b236
Takada-Aoi <a.m.3m0221@gmail.com>
2021-05-08 14:38:43 +0000

    [Finish]6

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites,dependent: :destroy
   has_many :book_comments,dependent: :destroy
-  
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
-  
+
   has_many :active_relationships,  class_name:  "Relationship",
                                    foreign_key: "followed_id",
                                    dependent:   :destroy
@@ -25,4 +25,16 @@ class User < ApplicationRecord
  def followed_by?(user)
   passive_relationships.find_by(followed_id: user.id).present?
  end
+
+ include JpPrefecture
+ jp_prefecture :prefecture_code
+
+ def prefecture_name
+   JpPrefecture::Prefecture.find(code: prefecture_code).try(:name)
+ end
+
+ def prefecture_name=(prefecture_name)
+   self.prefecture_code = JpPrefecture::Prefecture.find(name: prefecture_name).code
+ end
+
 end
